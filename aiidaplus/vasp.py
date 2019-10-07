c055c204890b25f6e18c461dc298ae92aca35b67
kei0822kei <mizokami.keiyu@gmail.com>
2019-10-07 16:17:00 +0900

    add phonopy

--- aiidaplus/vasp.py
+++ aiidaplus/vasp.py
@@ -6,6 +6,7 @@ aiida-vasp calculation toolkits
 -------------------------------
 definitions which help aiida-vasp calculation
 """
+import numpy as np
 
 def default_params(filetype, is_metal):
     """
@@ -15,7 +16,7 @@ def default_params(filetype, is_metal):
         ----------
         filetype : str
             input filetype setting file are exported
-            choose from 'oneshot', 'relax' or 'phonopy'
+            choose from 'oneshot', 'relax' or 'phonon'
         is_metal : bool
             if structure is metallic, choose True
             otherwise, choose False
@@ -33,6 +34,10 @@ def default_params(filetype, is_metal):
         ValueError
             conditions which ValueError occurs
     """
+    def _label_descrip(dic):
+        dic['label'] = "this is label"
+        dic['description'] = "this is description"
+
     def _structure(dic):
         dic['structure_pk'] = 'structure pk (int)'
 
@@ -70,44 +75,26 @@ def default_params(filetype, is_metal):
                   }
             incar_dic['incar_base'].update(static_opt)
 
-        def __incar_params_relax(incar_dic):
-            incar_dic['incar_relax'] = {
-                    'ediffg': -0.0001,
-                    'ibrion': 2,
-                    'nsw' : 20,
-                    'isif': 3
-                  }
+        # def __incar_params_relax(incar_dic):
+        #     incar_dic['incar_relax'] = {
+        #             'ediffg': -0.0001,
+        #             # 'ibrion': 2,
+        #             # 'nsw' : 20,
+        #             'isif': 3
+        #           }
 
         dic['incar'] = {}
         __incar_params_base(dic['incar'], is_metal)
-        __incar_params_relax(dic['incar'])
-
-    def _relax_conf(dic):
-        if filetype == 'relax':
-            dic['relax_conf'] = {
-                  'relax': True,
-                  # 'steps': 60,  # does not work? 2019/10/05
-                  # 'positions': True,  # does not work? 2019/10/05
-                  # 'shape': True,  # does not work? 2019/10/05
-                  # 'volume': True,  # does not work? 2019/10/05
-                  'convergence_on': True,
-                  'convergence_absolute': False,
-                  'convergence_max_iterations': 3,
-                  # 'convergence_shape_lengths': 0.1,
-                  'convergence_shape_lengths': 0.5,
-                  # 'convergence_shape_angles': 0.1,
-                  'convergence_shape_angles': 0.5,
-                  # 'convergence_volume': 0.01,
-                  'convergence_volume': 0.1,
-                  # 'convergence_positions': 0.01
-                  'convergence_positions': 0.1,
-            }
+        # __incar_params_relax(dic['incar'])
 
     def _kpoints(dic):
         dic['kpoints'] = {
               'mesh' : [6,6,6],
               'offset' : [0.5,0.5,0.5]
             }
+        if filetype == 'phonon':
+            dic['kpoints']['mesh_fc2'] = [2,2,2]
+            dic['kpoints']['mesh_nac'] = [12,12,12]
 
     def _potcar(dic):
         dic['potcar'] = {
@@ -118,11 +105,43 @@ def default_params(filetype, is_metal):
               }
           }
 
+    def _relax_conf(dic):
+        if filetype == 'relax':
+            dic['relax_conf'] = {
+                'relax': True,
+                'energy_cutoff': 1e-8,  # default, False
+                'force_cutoff': 1e-8,  # default, False
+                'steps': 20,  # default, 60
+                'positions': True,
+                'shape': True,
+                'volume': True,
+                'convergence_on': True,
+                'convergence_absolute': False,
+                'convergence_max_iterations': 3,
+                'convergence_volume': 0.01,
+                'convergence_positions': 0.01,
+                'convergence_shape_lengths': 0.1,
+                'convergence_shape_angles': 0.1
+              }
+
+    def _phonon_conf(dic):
+        if filetype == 'phonon':
+            dic['phonon_conf'] = {}
+            dic['phonon_conf']['phonon_settings'] = {
+                'supercell_matrix': [2,2,2],
+                'distance': 0.03,
+                'mesh': 50.0,  # ?? what is this setting?
+                'is_nac': True
+              }
+            dic['phonon_conf']['symmetry_tolerance'] = 1e-5
+
     dic = {}
+    _label_descrip(dic)
     _structure(dic)
     _clean_workingdir(dic)
     _incar_params(dic, is_metal)
     _kpoints(dic)
     _potcar(dic)
     _relax_conf(dic)
+    _phonon_conf(dic)
     return dic
--- scripts/aiidaplus-vasp.py
+++ scripts/aiidaplus-vasp.py
@@ -12,14 +12,14 @@ parser = argparse.ArgumentParser(
 parser.add_argument('-wf', '--workflow', type=str,
     default='vasp', help="choose workflow, 'vasp'(default), 'relax' or 'phonon'")
 parser.add_argument('--code', type=str,
-    default=None, help="input code ex. 'vasp544mpi'")
+    default=None, help="input code ex. 'vasp544mpi', 'phononpy'")
 parser.add_argument('--computer', type=str,
     default=None, help="input computer ex. 'vega'")
 parser.add_argument('--get_settings', type=str,
     default=None, help="get setting file, you don't have to set another parser \n \
                         input strings are \n \
                         'filetype' 'is_metal' 'filename' \n \
-                          filetype: choose from 'oneshot' 'relax' \n \
+                          filetype: choose from 'oneshot' 'relax' 'phonon'\n \
                           is_metal: bool \n \
                           filename: output filename")
 parser.add_argument('--params_yaml', type=str,
@@ -98,7 +98,9 @@ def main(code, computer, queue, verbose, wf, params_yaml):
     from aiida.common.extendeddicts import AttributeDict
     from aiida_vasp.utils.aiida_utils import get_data_class, get_data_node
 
-    def _unexpected_code(wf):
+    tot_num_mpiprocs = 16
+
+    def _unexpected_workflow():
         if wf is not ['vasp', 'relax', 'phonon']:
             raise ValueError("unexpected workflow: %s" % wf)
 
@@ -106,7 +108,7 @@ def main(code, computer, queue, verbose, wf, params_yaml):
         data = yaml.load(open(filename), Loader=Loader)
         return data
 
-    def _get_workflow(wf):
+    def _get_workflow():
         if wf == 'vasp':
             workflow = WorkflowFactory('vasp.vasp')
         elif wf =='relax':
@@ -114,27 +116,32 @@ def main(code, computer, queue, verbose, wf, params_yaml):
         elif wf == 'phonon':
             workflow = WorkflowFactory('phonopy.phonon')
         else:
-            _unexpected_code(code)
+            _unexpected_workflow()
         return workflow
 
-    def _set_computer_code(builder, code, computer):
+    def _set_computer_code(builder):
         if code in ['vasp544mpi', 'relax']:
             builder.code = Code.get_from_string('{}@{}'.format(code, computer))
-        elif code in ['phonon']:
-            builder.code_string = Code.get_from_string('{}@{}'.format(code, computer))
+        elif code in ['phonopy']:
+            # builder.code_string = Code.get_from_string('{}@{}'.format(code, computer))
+            builder.code_string = get_data_node('str', '{}@{}'.format(code, computer))
+        else:
+            raise ValueError("unexpected code: %s" % wf)
 
-    def _set_clean_workdir(builder, clean_workdir):
-        builder.clean_workdir = get_data_node('bool', clean_workdir)
+    def _set_clean_workdir(builder):
+        if wf in ['vasp', 'relax']:
+            builder.clean_workdir = get_data_node('bool', params['clean_workdir'])
 
-    def _set_kpoints(builder, kpoints):
+    def _set_kpoints(builder):
         """Set a simple kpoint sampling."""
-        kpt = get_data_class('array.kpoints')()
-        kpt.set_cell_from_structure(builder.structure)
-        kpt.set_kpoints_mesh(kpoints['mesh'], offset=kpoints['offset'])
-        builder.kpoints = kpt
+        if code in ['vasp544mpi', 'relax']:
+            kpt = get_data_class('array.kpoints')()
+            kpt.set_cell_from_structure(builder.structure)
+            kpt.set_kpoints_mesh(params['kpoints']['mesh'],
+                                 offset=params['kpoints']['offset'])
+            builder.kpoints = kpt
 
     def _set_options(builder):
-        tot_num_mpiprocs = 16
         options = AttributeDict()
         options.account = ''
         options.qos = ''
@@ -144,68 +151,150 @@ def main(code, computer, queue, verbose, wf, params_yaml):
         options.max_wallclock_seconds = 3600
         builder.options = get_data_node('dict', dict=options)
 
-    def _set_incar(builder, wf, incar):
-        builder.parameters = get_data_node('dict', dict=incar['incar_base'])
+    def _set_incar(builder):
+        if code in ['vasp544mpi', 'relax']:
+            builder.parameters = get_data_node(
+                    'dict', dict=params['incar']['incar_base'])
+            # if wf == 'relax':
+            #     builder.relax_parameters = get_data_node(
+            #             'dict', dict=params['incar']['incar_relax'])
+
+    def _set_description(builder):
+        builder.metadata.description = params['description']
+
+    def _set_label(builder):
+        builder.metadata.label = params['label']
+
+    def _set_potcar(builder):
+        if code in ['vasp544mpi', 'relax']:
+            builder.potential_family = \
+                    get_data_node('str', params['potcar']['potential_family'])
+            builder.potential_mapping = \
+                    get_data_node('dict', dict=params['potcar']['potential_mapping'])
+
+    def _set_relax_conf(builder):
         if wf == 'relax':
-            builder.relax_parameters = get_data_node('dict', dict=incar['incar_relax'])
+            keys = params['relax_conf'].keys()
+            builder.relax = \
+                    get_data_node('bool', params['relax_conf']['relax'])
+            if 'energy_cutoff' in keys:
+                builder.energy_cutoff = \
+                        get_data_node('float', params['relax_conf']['energy_cutoff'])
+            if 'force_cutoff' in keys:
+                builder.force_cutoff = \
+                        get_data_node('float', params['relax_conf']['force_cutoff'])
+            if 'steps' in keys:
+                builder.steps = \
+                        get_data_node('int', params['relax_conf']['steps'])
+            if 'positions' in keys:
+                builder.positions = \
+                        get_data_node('bool', params['relax_conf']['positions'])
+            if 'shape' in keys:
+                builder.shape = \
+                        get_data_node('bool', params['relax_conf']['shape'])
+            if 'volume' in keys:
+                builder.volume = \
+                        get_data_node('bool', params['relax_conf']['volume'])
+            if 'convergence_on' in keys:
+                builder.convergence_on = \
+                        get_data_node('bool', params['relax_conf']['convergence_on'])
+            if 'convergence_absolute' in keys:
+                builder.convergence_absolute = \
+                        get_data_node('bool', params['relax_conf']['convergence_absolute'])
+            if 'convergence_max_iterations' in keys:
+                builder.convergence_max_iterations = \
+                        get_data_node('int', params['relax_conf']['convergence_max_iterations'])
+            if 'convergence_shape_lengths' in keys:
+                builder.convergence_shape_lengths = \
+                        get_data_node('float', params['relax_conf']['convergence_shape_lengths'])
+            if 'convergence_shape_angles' in keys:
+                builder.convergence_shape_angles = \
+                        get_data_node('float', params['relax_conf']['convergence_shape_angles'])
+            if 'convergence_positions' in keys:
+                builder.convergence_positions = \
+                        get_data_node('float', params['relax_conf']['convergence_positions'])
+            if 'convergence_volume' in keys:
+                builder.convergence_volume = \
+                        get_data_node('float', params['relax_conf']['convergence_volume'])
 
-    def _set_potcar(builder, potcar):
-        builder.potential_family = \
-                get_data_node('str', potcar['potential_family'])
-        builder.potential_mapping = \
-                get_data_node('dict', dict=potcar['potential_mapping'])
+    def _set_phonon(builder):
+        def __get_config(is_nac):
+            dic = {}
+            base_config = {'code_string': 'vasp544mpi@'+computer,
+                           'potential_family': params['potcar']['potential_family'],
+                           'potential_mapping': params['potcar']['potential_mapping'],
+                           'options': {'resources': {'parallel_env': 'mpi*',
+                                                     'tot_num_mpiprocs': tot_num_mpiprocs},
+                                       'max_wallclock_seconds': 3600 * 10}}
+            base_parser_settings = {'add_energies': True,
+                                    'add_forces': True,
+                                    'add_stress': True}
+            forces_config = base_config.copy()
+            forces_config.update({'kpoints_mesh': params['kpoints']['mesh_fc2'],  # k-point density,
+                                  'parser_settings': base_parser_settings,
+                                  'parameters': params['incar']['incar_base']})
+            dic['forces'] = forces_config
 
-    def _set_relax_conf(builder, relax_conf):
-        builder.relax = \
-                get_data_node('bool', relax_conf['relax'])
-        # builder.steps = \
-        #         get_data_node('int', relax_conf['steps'])
-        # builder.positions = \
-        #         get_data_node('bool', relax_conf['positions'])
-        # builder.shape = \
-        #         get_data_node('bool', relax_conf['shape'])
-        # builder.volume = \
-        #         get_data_node('bool', relax_conf['volume'])
-        builder.convergence_on = \
-                get_data_node('bool', relax_conf['convergence_on'])
-        builder.convergence_absolute = \
-                get_data_node('bool', relax_conf['convergence_absolute'])
-        builder.convergence_max_iterations = \
-                get_data_node('int', relax_conf['convergence_max_iterations'])
-        builder.convergence_shape_lengths = \
-                get_data_node('float', relax_conf['convergence_shape_lengths'])
-        builder.convergence_shape_angles = \
-                get_data_node('float', relax_conf['convergence_shape_angles'])
-        builder.convergence_positions = \
-                get_data_node('float', relax_conf['convergence_positions'])
-        builder.convergence_volume = \
-                get_data_node('float', relax_conf['convergence_volume'])
+            if is_nac:
+                nac_config = base_config.copy()
+                nac_parser_settings = {'add_born_charges': True,
+                                       'add_dielectrics': True}
+                nac_parser_settings.update(base_parser_settings)
+                nac_incar_dict = {'lepsilon': True}
+                nac_incar_dict.update(params['incar']['incar_base'])
+                nac_config.update({'kpoints_mesh': params['kpoints']['mesh_nac'],
+                                   'parser_settings': nac_parser_settings,
+                                   'parameters': nac_incar_dict})
+                dic['nac'] = nac_config
 
-    def _set_structure(builder, structure_pk):
-        builder.structure = load_node(structure_pk)
+            return dic
+
+        if wf == 'phonon':
+            is_nac = params['phonon_conf']['phonon_settings']['is_nac']
+            builder.phonon_settings = get_data_node(
+                    'dict', dict=params['phonon_conf']['phonon_settings'])
+            builder.run_phonopy = get_data_node('bool', True)
+            builder.remote_phonopy = get_data_node('bool', True)
+            builder.calculator_settings = get_data_node(
+                    'dict', dict=__get_config(is_nac))
+
+    def _set_settings(builder):
+        if wf == 'relax':
+            builder.settings = get_data_node('dict', dict=
+                      {'add_energies': True,
+                       'add_forces': True,
+                       'add_stress': True}
+                    )
+
+
+
+    def _set_structure(builder):
+        builder.structure = load_node(params['structure_pk'])
 
     def _set_verbose(builder):
-        builder.verbose = get_data_node('bool', verbose)
+        if wf in ['vasp', 'relax']:
+            builder.verbose = get_data_node('bool', verbose)
 
     ### build
     params = _load_yaml(params_yaml)
-    workflow = _get_workflow(wf)
+    workflow = _get_workflow()
     builder = workflow.get_builder()
-    _set_structure(builder, params['structure_pk'])
-    _set_computer_code(builder, code, computer)
+    _set_structure(builder)
+    _set_computer_code(builder)
     _set_options(builder)
-    _set_potcar(builder, params['potcar'])
-    _set_kpoints(builder, params['kpoints'])
-    _set_incar(builder, wf, params['incar'])
-    if wf == 'relax':
-        _set_relax_conf(builder, params['relax_conf'])
+    _set_potcar(builder)
+    _set_kpoints(builder)
+    _set_incar(builder)
+    _set_relax_conf(builder)
     _set_verbose(builder)
-    _set_clean_workdir(builder, params['clean_workdir'])
+    _set_clean_workdir(builder)
+    _set_label(builder)
+    _set_settings(builder)
+    _set_phonon(builder)
 
     ### run
     # run(workflow, **builder)
-    run(workflow, **builder)
-    # submit(workflow, **builder)
+    submit(workflow, **builder)
 
 
 if __name__ == '__main__':
